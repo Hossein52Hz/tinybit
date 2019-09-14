667f66dfaa6b2253f336e41ccf15034d7d64d623
Hossein52Hz <Hossein52Hz@gmail.com>
2019-09-14 10:29:54 +0430

    update tags

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -32,24 +32,11 @@ defined( 'ABSPATH' ) || exit;
 
 	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 
-	<div class="entry-content">
-
-		<?php the_excerpt(); ?>
-
-		<?php
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-
-	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
 
-		<?php understrap_entry_footer(); ?>
+		<?php // understrap_entry_footer(); 
+		the_tags('', '', '');?>
 
 	</footer><!-- .entry-footer -->
 
