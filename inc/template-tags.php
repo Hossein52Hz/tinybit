2fe59b8d8f1c706ed46a3181cebcebd9a285e25d
Hossein52Hz <Hossein52Hz@gmail.com>
2019-09-17 01:28:12 +0430

    remove display update date

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -16,7 +16,7 @@ defined( 'ABSPATH' ) || exit;
 if ( ! function_exists( 'understrap_posted_on' ) ) {
 	function understrap_posted_on() {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) && !is_front_page()) {
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) && !is_front_page() && !is_archive() && !is_search() ) {
 			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s"> (%4$s) </time>';
 		}
 		$time_string = sprintf( $time_string,
