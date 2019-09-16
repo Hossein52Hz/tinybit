5197fb177dad8c377f9c9c1122b665af11f78dae
Hossein52Hz <Hossein52Hz@gmail.com>
2019-09-17 01:29:26 +0430

    make nav-bar UI

--- header.php
+++ header.php
@@ -30,47 +30,69 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary">
+		 <nav class="navbar navbar-expand-md navbar-dark">
 
 		<?php if ( 'container' == $container ) : ?>
 			<div class="container">
 		<?php endif; ?>
 
-					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
-
-						<?php if ( is_front_page() && is_home() ) : ?>
-
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
-
-						<?php else : ?>
-
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-
-						<?php endif; ?>
-
-
-					<?php } else {
-						the_custom_logo();
-					} ?><!-- end custom logo -->
-
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 					<span class="navbar-toggler-icon"></span>
 				</button>
+			<div id="navbarNavDropdown" class="header-menu navbar-collapse flex-column collapse">
+				<!-- Your site title as branding in the menu -->
+				<?php if ( ! has_custom_logo() ) { ?>
 
+				<?php if ( is_front_page() && is_home() ) : ?>
+
+					<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+
+				<?php else : ?>
+
+					<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+
+				<?php endif; ?>
+
+
+				<?php } else {
+				the_custom_logo();
+				} ?><!-- end custom logo -->
+
+				<div class="author-info">
+				
+				<?php if ( get_theme_mod( 'understrap_author_options_full_name' ) ): ?>
+              		<h2 class="full-name"><?php echo esc_attr(get_theme_mod( 'understrap_author_options_full_name' )); ?></h2>
+            	<?php endif; ?>
+				
+				<?php if ( get_theme_mod( 'understrap_author_options_career' ) ): ?>
+              		<p class="career"><?php echo esc_attr(get_theme_mod( 'understrap_author_options_career' )); ?></p>
+            	<?php endif; ?>
+				
+				<!-- <p class="line-top"></p> -->
+				<ul class="social-media">
+				
+				<?php show_social_media(); ?>
+
+				</ul>
+				<p class="line"></p>
+				</div>
 				<!-- The WordPress Menu goes here -->
 				<?php wp_nav_menu(
 					array(
 						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav ml-auto',
+						'container_class' => 'flex-column',
+						// 'container_id'    => '',
+						'menu_class'      => 'navbar-nav flex-column text-left',
 						'fallback_cb'     => '',
 						'menu_id'         => 'main-menu',
 						'depth'           => 2,
 						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
 					)
 				); ?>
+
+
+			</div>
+
 			<?php if ( 'container' == $container ) : ?>
 			</div><!-- .container -->
 			<?php endif; ?>
