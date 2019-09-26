9dd2cb396f50498aea8b0067708a25452f1b7d5c
Hossein52Hz <Hossein52Hz@gmail.com>
2019-09-27 01:33:53 +0330

    fix 404 style

--- 404.php
+++ 404.php
@@ -20,7 +20,17 @@ $container = get_theme_mod( 'tinybit_container_type' );
 		<div class="row">
 
 			<div class="col-md-12 content-area" id="primary">
-
+			<div id="site-info">
+				<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+				<p class="description">
+				<?php bloginfo( 'description' ); ?>
+				</p>			
+				<div>
+					<ul class="social-link">
+					<?php show_social_media(); ?>
+					</ul>
+				</div>
+			</div>
 				<main class="site-main" id="main">
 
 					<section class="error-404 not-found">
@@ -36,41 +46,6 @@ $container = get_theme_mod( 'tinybit_container_type' );
 							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'tinybit' ); ?></p>
 
 							<?php get_search_form(); ?>
-
-							<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-							<?php if ( tinybit_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-
-								<div class="widget widget_categories">
-
-									<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'tinybit' ); ?></h2>
-
-									<ul>
-										<?php
-										wp_list_categories(
-											array(
-												'orderby'    => 'count',
-												'order'      => 'DESC',
-												'show_count' => 1,
-												'title_li'   => '',
-												'number'     => 10,
-											)
-										);
-										?>
-									</ul>
-
-								</div><!-- .widget -->
-
-							<?php endif; ?>
-
-							<?php
-
-							/* translators: %1$s: smiley */
-							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'tinybit' ), convert_smilies( ':)' ) ) . '</p>';
-							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-							the_widget( 'WP_Widget_Tag_Cloud' );
-							?>
 
 						</div><!-- .page-content -->
 
